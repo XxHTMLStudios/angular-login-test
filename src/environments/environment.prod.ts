16709dba9ccd48ae7a9b919488e704c352134a8d
XxHTML <isaacdelucas@gmail.com>
2021-07-11 15:41:54 +0200

    idk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  login: process.env.LOGIN_PASS
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  login: process.env.LOGIN_PASS
+  production: false
 };
 
 /*
