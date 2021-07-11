57eab126d220438cda54e67fbc3d1c663247a166
marosca <mariolamoreno@hotmail.es>
2021-07-11 21:58:02 +0200

    update server

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/login-angular'));
+app.use(express.static(__dirname + "/dist/login-angular"));
 
-// Send all requests to index.html
-app.get('', function(req, res) {
-    console.log("*****************" + process.env.LOGIN_PASS)
-  res.sendFile(path.join(__dirname + '/dist/login-angular/index.html'));
+// urlencode
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// api to ensure that the use has correct pass
+app.post("/api/login", (req, res) => {
+  const correctPass = process.env.LOGIN_PASS;
+  const inputPass = req.body.pass;
+  console.log("***", correctPass);
+  if (correctPass === inputPass) {
+    res.status(200).json({
+      isValid: true,
+    });
+  } else {
+    res.status(404).json({
+      isValid: false,
+    });
+  }
+});
+
+app.get(/.*/, function (req, res) {
+  console.log("*****************" + process.env.LOGIN_PASS);
+  res.sendFile(path.join(__dirname + "/dist/login-angular/index.html"));
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(
+    `Node Express server listening on http://localhost:${
+      process.env.PORT || 5000
+    }`
+  );
+});
